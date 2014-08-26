e2a3441facf4ca96b09219cbe533fd8ebe0ac131
Steven Inouye <steven.inouye91@gmail.com>
2014-08-26 03:14:07 -0700

    wow I did it -adding dates correctly -displaying dates on home page

--- public/js/controllers/DateCtrl.js
+++ public/js/controllers/DateCtrl.js
@@ -6,4 +6,17 @@ angular.module('DateCtrl', []).controller('DateController', function($scope, $ht
     console.log(dates)
   });
 
+  $scope.createDate = function(){
+    $http.post('/api/dates', $scope.formData)
+    .success(function(data){
+    console.log($scope.formData);
+      $scope.formData = {};
+      $scope.dates = data;
+      console.log(data);
+    })
+    .error(function(data){
+      console.log('Error:' + data);
+    });
+  };
+
 });
