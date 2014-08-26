29d1b55be8ea83ec1ae73d1624cd606ae4bd74a1
Steven Inouye <steven.inouye91@gmail.com>
2014-08-25 21:59:33 -0700

    the beginning again

---
+++ app/js/controllers.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/* Controllers */
+
+var banana = angular.module('myApp.controllers', [])
+
+banana.controller('MyCtrl1', ['$scope','$http', function($scope, $http) {
+    //this == controller
+    $http.get('dates/dates.json').success(function(data){
+      this.dates = data;
+    }.bind(this));
+
+    this.date = {};
+
+
+
+    this.addPost = function(controller){
+      console.log(controller.date)
+      console.log(this)
+      this.dates.push(controller.date)
+      controller.date = {};
+    };
+
+    $scope.orderProp = '-day'
+  }])
+
+
+  banana.controller('MyCtrl2', ['$scope', function($scope) {
+
+  }]);
+  // .controller('DateController', function(){
+  //   this.date = {};
+  //   this.addNewDate = function(product){
+  //     console.log(this.product)
+  //   };
+  // });
